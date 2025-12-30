796bc0bf2d9161b94f80001d750f58b8bf690972
ali aghaie <4liaghaie@gmail.com>
2025-12-30 21:53:40 +0330

    image-fix3

--- my-portfolio-frontend/next.config.mjs
+++ my-portfolio-frontend/next.config.mjs
@@ -2,6 +2,12 @@
 const nextConfig = {
   images: {
     domains: ["api.muhsinzade.com", "localhost", "127.0.0.1", "images.unsplash.com"],
+    remotePatterns: [
+      { protocol: "https", hostname: "api.muhsinzade.com" },
+      { protocol: "https", hostname: "images.unsplash.com" },
+      { protocol: "http", hostname: "localhost" },
+      { protocol: "http", hostname: "127.0.0.1" },
+    ],
   },
 };
 
